8cec91d361c603e05a36a29cd1b2427cb0fbbdf1
Riley Mathews <rileymathews80@gmail.com>
2019-06-28 19:24:02 -0500

    switched to using pytest

--- test/test_main.py
+++ test/test_main.py
@@ -1,11 +1,10 @@
-from src.main import lambda_handler
-from unittest import TestCase
+from src.lambda_function import lambda_handler
 
 
-class TestHandler(TestCase):
+class TestHandler(object):
     def test_can_return_message(self):
         event = {"message": "This is a test message"}
-        self.assertEqual(
-            lambda_handler(event, None),
-            {"return_message": "This is a test message", "config": {"foo": "bar"}},
-        )
+        assert lambda_handler(event, None) == {
+            "return_message": "This is a test message",
+            "config": {"foo": "bar"},
+        }
