f7ad5509e2579a40d77d4f4247f1ce6c131ac434
Riley Mathews <rileymathews80@gmail.com>
2019-07-11 21:16:05 -0500

    added actions to lambda

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,7 +1,20 @@
 import json
+import sys
 
 CONFIGURATION = {"foo": "bar"}
 
 
 def lambda_handler(event, context):
-    return {"return_message": event["message"], "config": CONFIGURATION}
+    return getattr(sys.modules[__name__], event["method"])(event["first_number"], event["second_number"])
+
+def add(first, second):
+    return first + second
+
+def subtract(first, second):
+    return first - second
+
+def multiply(first, second):
+    return first * second
+
+def divide(first, second):
+    return first / second
--- test/test_main.py
+++ test/test_main.py
@@ -2,9 +2,18 @@ from src.lambda_function import lambda_handler
 
 
 class TestHandler(object):
-    def test_can_return_message(self):
-        event = {"message": "This is a test message"}
-        assert lambda_handler(event, None) == {
-            "return_message": "This is a test message",
-            "config": {"foo": "bar"},
-        }
+    def test_can_add_numbers(self):
+        event = {"method": "add", "first_number": 1, "second_number": 2}
+        assert lambda_handler(event, None) == 3
+
+    def test_can_subtract_numbers(self):
+        event = {"method": "subtract", "first_number": 5, "second_number": 2}
+        assert lambda_handler(event, None) == 3
+
+    def test_can_multiply_numbers(self):
+        event = {"method": "multiply", "first_number": 3, "second_number": 2}
+        assert lambda_handler(event, None) == 6
+
+    def test_can_divide_numbers(self):
+        event = {"method": "divide", "first_number": 6, "second_number": 2}
+        assert lambda_handler(event, None) == 3
